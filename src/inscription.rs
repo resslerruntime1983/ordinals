beeefb966dc3b57b817751465aacf784185556c1
Casey Rodarmor <casey@rodarmor.com>
2023-08-23 13:33:11 -0700

    Add a bunch of tests

--- src/inscription.rs
+++ src/inscription.rs
@@ -24,7 +24,7 @@ pub(crate) enum Curse {
   Reinscription,
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone, Default)]
 pub struct Inscription {
   body: Option<Vec<u8>>,
   content_type: Option<Vec<u8>>,
@@ -834,6 +834,156 @@ mod tests {
     assert_eq!(
       InscriptionParser::parse(&envelope(&[b"ord", &[2], &[0]])),
       Err(InscriptionError::UnrecognizedEvenField),
+    );
+  }
+
+  #[test]
+  fn inscription_with_no_parent_field_has_no_parent() {
+    assert!(Inscription {
+      parent: None,
+      ..Default::default()
+    }
+    .parent()
+    .is_none());
+  }
+
+  #[test]
+  fn inscription_with_parent_field_shorter_than_txid_length_has_no_parent() {
+    assert!(Inscription {
+      parent: Some(vec![]),
+      ..Default::default()
+    }
+    .parent()
+    .is_none());
+  }
+
+  #[test]
+  fn inscription_with_parent_field_longer_than_txid_and_index_has_no_parent() {
+    assert!(Inscription {
+      parent: Some(vec![1; 37]),
+      ..Default::default()
+    }
+    .parent()
+    .is_none());
+  }
+
+  #[test]
+  fn inscription_with_parent_field_index_with_trailing_zeroes_has_no_parent() {
+    let mut parent = vec![1; 36];
+
+    parent[35] = 0;
+
+    assert!(Inscription {
+      parent: Some(parent),
+      ..Default::default()
+    }
+    .parent()
+    .is_none());
+  }
+
+  #[test]
+  fn inscription_parent_txid_is_deserialized_correctly() {
+    assert_eq!(
+      Inscription {
+        parent: Some(vec![
+          0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+          0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d,
+          0x1e, 0x1f,
+        ]),
+        ..Default::default()
+      }
+      .parent()
+      .unwrap()
+      .txid,
+      "1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a09080706050403020100"
+        .parse()
+        .unwrap()
+    );
+  }
+
+  #[test]
+  fn inscription_parent_with_zero_byte_index_field_is_deserialized_correctly() {
+    assert_eq!(
+      Inscription {
+        parent: Some(vec![1; 32]),
+        ..Default::default()
+      }
+      .parent()
+      .unwrap()
+      .index,
+      0
+    );
+  }
+
+  #[test]
+  fn inscription_parent_with_one_byte_index_field_is_deserialized_correctly() {
+    assert_eq!(
+      Inscription {
+        parent: Some(vec![
+          0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+          0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+          0xff, 0xff, 0x01
+        ]),
+        ..Default::default()
+      }
+      .parent()
+      .unwrap()
+      .index,
+      1
+    );
+  }
+
+  #[test]
+  fn inscription_parent_with_two_byte_index_field_is_deserialized_correctly() {
+    assert_eq!(
+      Inscription {
+        parent: Some(vec![
+          0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+          0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+          0xff, 0xff, 0x01, 0x02
+        ]),
+        ..Default::default()
+      }
+      .parent()
+      .unwrap()
+      .index,
+      0x0201,
+    );
+  }
+
+  #[test]
+  fn inscription_parent_with_three_byte_index_field_is_deserialized_correctly() {
+    assert_eq!(
+      Inscription {
+        parent: Some(vec![
+          0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+          0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+          0xff, 0xff, 0x01, 0x02, 0x03
+        ]),
+        ..Default::default()
+      }
+      .parent()
+      .unwrap()
+      .index,
+      0x030201,
+    );
+  }
+
+  #[test]
+  fn inscription_parent_with_four_byte_index_field_is_deserialized_correctly() {
+    assert_eq!(
+      Inscription {
+        parent: Some(vec![
+          0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+          0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+          0xff, 0xff, 0x01, 0x02, 0x03, 0x04,
+        ]),
+        ..Default::default()
+      }
+      .parent()
+      .unwrap()
+      .index,
+      0x04030201,
     );
   }
 }
